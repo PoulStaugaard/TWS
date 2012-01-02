c9662411bf51e32e077afa1bad550d614ec2d081
Poul Staugaard <poul.staugaard@gmail.com>
2012-01-02 23:34:02 +0100

    Support also (Chrome on) Win7-x64 and Linux

--- browser.js
+++ browser.js
@@ -1,9 +1,14 @@
 // this: browser.js
-// does: Starts a simple node.js server and a browser to hit it
-// ver.: 0.1.0
+// is:   Platform dependencies related to the browser launch command
+// ver.: 0.2.2
 // by:   Poul Staugaard
 // URL:  http://giewiki.appspot.com/Tutorials/nodepad
 
-exports.command = process.env.USERPROFILE ?
-    '"' + process.env.USERPROFILE + '/Local Settings/Application Data/Google/Chrome/Application/chrome.exe" --homepage ' :
-    "open '/Applications/Google Chrome.app' --args --homepage ";
+if (process.env.USERPROFILE) // i.e. Windows
+	exports.command = process.platform == 'win32' ? 
+	    '"' + process.env.USERPROFILE + '/Local Settings/Application Data/Google/Chrome/Application/chrome.exe" --homepage ' :
+	    '"C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe" --homepage ';
+else if (process.platform == 'darwin') // i.e. Mac
+	exports.command = "open '/Applications/Google Chrome.app' --args --homepage ";
+else
+	exports.command = "/usr/bin/google-chrome";
