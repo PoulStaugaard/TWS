a1accbfb49359384baf58f1359eedd24497f0f0d
Poul Staugaard <poul.staugaard@gmail.com>
2012-01-05 08:48:30 +0100

    improved browser path search on Windows

--- browser.js
+++ browser.js
@@ -1,13 +1,38 @@
 // this: browser.js
 // is:   Platform dependencies related to the browser launch command
-// ver.: 0.2.2
+// ver.: 0.2.3
 // by:   Poul Staugaard
 // URL:  http://giewiki.appspot.com/Tutorials/nodepad
 
-if (process.env.USERPROFILE) // i.e. Windows
-	exports.command = process.platform == 'win32' ? 
-	    '"' + process.env.USERPROFILE + '/Local Settings/Application Data/Google/Chrome/Application/chrome.exe" --homepage ' :
-	    '"C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe" --homepage ';
+if (process.env.USERPROFILE) { // i.e. Windows
+	var exepath = '/Google/Chrome/Application/chrome.exe';
+	var fx = function(p) {
+		var fs = require('fs');
+		try {
+			p = p + exepath;
+			fs.statSync(p);
+			return p;
+		}
+		catch (x)
+		{
+			exports.tried = exports.tried || [];
+			exports.tried.push(p);
+			return false;
+		}
+	}
+	
+	var otherpath = process.env.GooglePath || process.env['ProgramFiles(x86)'];
+	exports.command =
+		fx(process.env.ProgramFiles) || 
+		fx(process.env.APPDATA) ||
+		fx(process.env.USERPROFILE + "/Local Settings/Application Data") ||
+		(otherpath && fx(otherpath));
+		
+	if (exports.command) {
+		console.log("Using " + exports.command);
+		exports.command = '"' + exports.command + '" --homepage ';
+	}
+}
 else if (process.platform == 'darwin') // i.e. Mac
 	exports.command = "open '/Applications/Google Chrome.app' --args --homepage ";
 else
--- tws.js
+++ tws.js
@@ -9,15 +9,20 @@ http.createServer(dispatcher).listen(1337, "127.0.0.1");
 
 var exec = require('child_process').exec;
 var docfile = process.argv.length > 2 ? process.argv[2] : "";
-var cmd = require('./browser.js').command + ' "' + 'http://localhost:1337/' + docfile + '"';
-exec(cmd,function(err,stdout,stderr) {
-            if (err) {
-                console.log("Failed to start " + cmd);
-                console.dir(err);
-            }
-			else
-				console.log("Started " + cmd);
-		}
-    );
+var browser = require('./browser.js');
+if (!browser.command)
+	console.log("Browser not found, tried \n" + browser.tried.join('\n'));
+else {
+	var cmd = browser.command + ' "' + 'http://localhost:1337/' + docfile + '"';
+	exec(cmd,function(err,stdout,stderr) {
+				if (err) {
+					console.log("Failed to start " + cmd);
+					console.dir(err);
+				}
+				else
+					console.log("Started " + cmd);
+			}
+		);
+}
 console.log("Ctrl+C terminates process");
 
